bc8d2c5302f027332316042424d9d80281e2ffc0
idris.aitmoulay <idris.aitmoulay@gmail.com>
2020-03-14 18:14:19 +0100

    isPalindrome

--- palindrome/utils.js
+++ palindrome/utils.js
@@ -1,14 +1,21 @@
+/**
+ * Runtime: 176 ms, faster than 91.42% of JavaScript online submissions for Palindrome Number.
+ * Memory Usage: 45.6 MB, less than 58.62% of JavaScript online submissions for Palindrome Number.
+ */
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
 const isPalindrome = x => {
-  const input = x + '';
-  const str = input.split("").reverse().join("");
+  const input = (x + '').split("");
+  const str = [...input].reverse();
 
   let isNot = true;
-  for (let i=0; i < str.split("").length/2; i++) {
+  for (let i=0; i < input.length / 2; i++) {
     if (str[i]!==input[i]) {
       isNot = false;
       break;
     }
   }
-
   return isNot;
 };
